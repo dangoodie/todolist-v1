fc90f191e73b4ec24e574853b6bcbeb4c58caa17
dangoodie <70499319+dangoodie@users.noreply.github.com>
2022-07-24 17:56:22 +1000

    Items able to be added onto list

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,13 +16,18 @@ app.get("/", function (req, res) {
     }
   var today = new Date();
   var dayName = today.toLocaleDateString("en-US", options);
+  
 
-  res.render("list", { dayName: dayName });
+  res.render("list", { 
+    dayName: dayName,
+    items: items
+ });
 });
 
-app.post("/", function(req) {
-    var newListItem = req.body.newListItem;
-    console.log(newListItem); 
+app.post("/", function(req, res) {
+    var item = req.body.item;
+    items.push(item);
+    res.redirect("/");
 })
 
 app.listen(port, function () {
