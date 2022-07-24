e60a7cdd3c4d5285b2cc7cdf0692016fc650ed38
dangoodie <70499319+dangoodie@users.noreply.github.com>
2022-07-24 20:24:36 +1000

    create work list

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,21 +14,34 @@ app.get("/", function (req, res) {
   let options = {
     weekday: "long",
     day: "numeric",
-    month: "long"
+    month: "long",
   };
   let today = new Date();
   let dayName = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {
-    dayName: dayName,
-    items: items,
-  });
+  res.render("list", { listTitle: dayName, items: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.item;
-  items.push(item);
-  res.redirect("/");
+  
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", items: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.item;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(port, function () {
