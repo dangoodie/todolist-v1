ae9c92da15f9d9b1b91c51065dc5f77f41057522
dangoodie <70499319+dangoodie@users.noreply.github.com>
2022-07-24 18:03:56 +1000

    formatting

--- app.js
+++ app.js
@@ -6,29 +6,28 @@ const port = 3000;
 var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    var options = { 
-        weekday: "long", 
-        day: "numeric", 
-        month: "long"
-    }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
   var today = new Date();
   var dayName = today.toLocaleDateString("en-US", options);
-  
 
-  res.render("list", { 
+  res.render("list", {
     dayName: dayName,
-    items: items
- });
+    items: items,
+  });
 });
 
-app.post("/", function(req, res) {
-    var item = req.body.item;
-    items.push(item);
-    res.redirect("/");
-})
+app.post("/", function (req, res) {
+  var item = req.body.item;
+  items.push(item);
+  res.redirect("/");
+});
 
 app.listen(port, function () {
   console.log("Server is listening on port " + port);
